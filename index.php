959807985c163c8d3bf62155535558067cb01d3b
CynicalHornet <38018717+CynicalHornet@users.noreply.github.com>
2018-04-02 22:58:50 +0100

    0.02

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'src/twitter.class.php';
-$consumerKey = $_ENV["CONSUMERTOKEN"]
-$consumerSecret = $_ENV["CONSUMERSECRET"]
-$accessToken = $_ENV["ACCESSTOKEN"]
-$accessTokenSecret = $_ENV["ACCESSSECRET"]
+$consumerKey = $_ENV["CONSUMERTOKEN"];
+$consumerSecret = $_ENV["CONSUMERSECRET"];
+$accessToken = $_ENV["ACCESSTOKEN"];
+$accessTokenSecret = $_ENV["ACCESSSECRET"];
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 try {
 	$date = date('l jS \of F Y h:i:s A');
